3a077b430d456dedcad40dc3d0519ad6476ab23a
hector_saucedo <hector.saucedo@oracle.com>
2022-11-24 21:49:31 -0600

    kafka support

--- master/jupyter.sh
+++ master/jupyter.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker exec -it spark-master /spark/bin/pyspark --master spark://spark-master:7077
+lib_path=/work/lib
+docker exec -it spark-master /spark/bin/pyspark --master spark://spark-master:7077 --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.0 --driver-class-path $lib_path/sqlite-jdbc-3.39.3.0.jar --jars $lib_path/sqlite-jdbc-3.39.3.0.jar
